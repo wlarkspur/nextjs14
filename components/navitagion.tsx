9f780e7bc35e367802e9fe763e691a76400e9028
wlarkspur <newacon@naver.com>
2024-02-02 20:27:32 +0200

    next teory part

--- components/navitagion.tsx
+++ components/navitagion.tsx
@@ -1,10 +1,12 @@
 "use client";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function Navigation() {
   const path = usePathname();
-
+  const [count, setCount] = useState(0);
   return (
     <nav>
       <ul>
@@ -14,6 +16,14 @@ export default function Navigation() {
         <li>
           <Link href={"/about-us"}>About Us</Link>
           {path === "/about-us" ? "🔥" : ""}
+        </li>
+        <li>
+          <button
+            style={{ padding: "100px" }}
+            onClick={() => setCount((c) => c + 1)}
+          >
+            {count}
+          </button>
         </li>
       </ul>
     </nav>

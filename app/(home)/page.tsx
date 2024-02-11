edf938f59ba48f90d0890f9eaf289a4dbf4f0f85
wlarkspur <newacon@naver.com>
2024-02-11 12:50:41 +0200

    setting

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import styles from "../../styles/home.module.css";
+import Movie from "../../components/movie";
 
 export const metadata = {
   title: "Home",
@@ -16,11 +17,13 @@ async function getMovies() {
 export default async function HomePage() {
   const movies = await getMovies();
   return (
-    <div>
+    <div className={styles.container}>
       {movies.map((movie) => (
-        <li key={movie.id}>
-          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
+        <Movie
+          title={movie.title}
+          id={movie.id}
+          poster_path={movie.poster_path}
+        />
       ))}
     </div>
   );
